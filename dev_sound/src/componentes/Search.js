404b2cfe5a83b8a83e89724c0587102e50be1750
Gabriel Trindade da Conceição <trindadi13@gmail.com>
2021-06-16 09:27:09 -0300

    correçoes na search

--- dev_sound/src/componentes/Search.js
+++ dev_sound/src/componentes/Search.js
@@ -1,12 +1,15 @@
 import React from 'react'
-import {StyleSheet, TextInput, Image, View} from 'react-native'
+import {StyleSheet, TextInput, Image, View, TouchableOpacity} from 'react-native'
 import searchIcon from '../../assets/icons/search_icon.png'
 
 export default props => {
     return (
         <View style = {styles.container}>
             <TextInput style = {styles.input} placeholder = 'O que você procura hoje?'/>
-            <Image style = {styles.image} source = {searchIcon}/>
+            <TouchableOpacity>
+                <Image style = {styles.image} source = {searchIcon}/>
+            </TouchableOpacity>
+            
         </View>
     )
 }
@@ -14,14 +17,19 @@ const styles = StyleSheet.create({
     container : {
         flexDirection: 'row',
         backgroundColor: '#c1c1c1',
-        padding: 10,
-        borderRadius:8
+        padding: 5,
+        borderRadius:8,
+        
     },
     image : {
         backgroundColor: '#c1c1c1',
-        marginLeft: 20
+        marginLeft: 20,
+        marginRight: 10
+        
     },
     input : {
-        width: 'auto'
+        width: 250,
+        fontSize: 19,
+        marginLeft: 15
     }
 })
