7562382f4ad087679a1252c9b83bd66db18a0318
Gabriel Trindade da Conceição <trindadi13@gmail.com>
2021-06-28 09:14:56 -0300

    jsons

--- dev_sound/src/models/contato.js
+++ dev_sound/src/models/contato.js
@@ -4,11 +4,11 @@ module.exports = app => {
     const schemaContato = mongoose.Schema
     (
         {
-            "id_contato" : {type: String, required: true, index:{unique: true}},
-            "nome": {type : String,  required: true},
-            "email": {type: String, required: true},
-            "assunto": {type: String, required: true},
-            "mensagem": {type: String, required: true} 
+            id_contato : {type: String, required: true, index:{unique: true}},
+            nome: {type : String,  required: true},
+            email: {type: String, required: true},
+            assunto: {type: String, required: true},
+            mensagem: {type: String, required: true} 
         }
         
     )
