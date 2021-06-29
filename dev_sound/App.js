e69dc9ad2b2ea39050f192bd0fe8bc6a6752ad08
Andry R Santos <anrodriguessantos0@gmail.com>
2021-06-29 20:02:37 -0300

    componente text input

--- dev_sound/App.js
+++ dev_sound/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { SafeAreaView,StyleSheet } from 'react-native';
 import Home from './src/Views/screens/Home';
+import Input from './src/Views/components/Input';
+
 
 export default () => {
   return (
      <SafeAreaView style={style.container} >
         
         {/* Begin Pagina Home   */}
-        <Home/>
+        
+        <Input
+          label='Número do Crédito '
+          placeholder='Insira o nome'
+
+        /> 
         {/* End Pagina Home   */}
 
-        
       </SafeAreaView>
     )
 
