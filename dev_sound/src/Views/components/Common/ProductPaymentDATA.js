0cd02fab9b33017b1ed9a0b405681e6f7ea23433
Gabriel Trindade da Conceição <trindadi13@gmail.com>
2021-07-14 15:32:58 -0300

    splash art

--- dev_sound/src/Views/components/Common/ProductPaymentDATA.js
+++ dev_sound/src/Views/components/Common/ProductPaymentDATA.js
@@ -1,10 +1,10 @@
-import ImagesProject from './ImagesProject'
+
 
 export default [
 
     {
         id:1,
-        imgProduct:ImagesProject.ProductImages.violaoTakamine,
+        imgProduct:"https://i.ibb.co/njLqjDX/violao-harmonics-ge21.png",
         nameProduct:'Violão Takamine',
         priceUnit:parseFloat(678),
         modelProduct:'AX00123-23',
